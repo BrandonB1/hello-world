4ba4f2d7574903870dd1e212b21bed9c95f04752
BrandonB1 <brandonb2019@gmail.com>
2014-09-14 02:23:56 -0500

    Spellcheck
    Fixed a semantic error in the documentation.

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 
 
-int main(int argc, const char * argv[]) // the function main is where a C program begins and ends, and it is of type int, meaining it returns an integer. The parenthesis enclose command line arguments, and within the curly braces, is the code.
+int main(int argc, const char * argv[]) // the function main is where a C program begins and ends, and it is of type int, meaining it returns an integer. The parenthesis enclose command line arguments, and within the braces, is the code.
 {
     printf("Hello World!"); // prints the arguments to the screen, what is in quotation marks
     return 0; // ends the program
